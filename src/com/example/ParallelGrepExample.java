ae770c9a22877de99d712e43ce8b12a5f456813a
oresztesz-margaritisz <oresztesz_margaritisz@epam.com>
2017-01-06 12:03:08 +0100

    Parallel grep with additional tweaks

--- src/com/example/ParallelGrepExample.java
+++ src/com/example/ParallelGrepExample.java
@@ -4,7 +4,9 @@ import java.io.IOException;
 import java.nio.file.*;
 
 /**
- * Created by Oresztesz_Margaritis on 1/6/2017.
+ * Example showing how to perform grep in parallel using streams.
+ * Note: Files.lines has some performance problems and will be fixed in Java 9.
+ * http://bytefish.de/blog/jdk8_files_lines_parallel_stream/
  */
 public class ParallelGrepExample {
 
@@ -15,9 +17,8 @@ public class ParallelGrepExample {
         final String needle = "RootTaskAsync";
         Files.walk(start, 100).parallel().filter(path -> path.toString().endsWith(".java")).forEach(path -> {
             try {
-                Files.lines(path).parallel().filter(line -> line.contains(needle)).forEach(line -> {
-                    System.out.println("Match in " + path.toString() + " found: \n" + line + "\n ... ");
-                });
+                Files.lines(path).parallel().filter(line -> line.contains(needle)).forEach(line ->
+                        System.out.println("Match in " + path.toString() + " found: \n" + line + "\n ... "));
             } catch (IOException e) {
                 e.printStackTrace();
             }
